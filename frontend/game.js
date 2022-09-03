254a1033ee70236b1a9cbf0472361a32dd826d1a
LorenzoRicci26 <lorenzo.ricci15@stud.unifi.it>
2022-09-03 11:54:57 +0200

    Impostato timer con alert finale

--- frontend/game.js
+++ frontend/game.js
@@ -15,9 +15,53 @@ $(async () => {
   const camCanvas2 = createPoseCanvas($("#camCanvas2").get(0));
   const imgCanvas = createPoseCanvas($("#imgCanvas").get(0));
 
+  
+  
   const queryParams = new URLSearchParams(window.location.search);
 
   const levelId = queryParams.get("id");
 
   initGame(levelId, video, camCanvas1, camCanvas2, imgCanvas);
+
+  //Codice per il timer
+  var minutes = 5;
+  var seconds = 0;
+  var timestart = false;
+  document.getElementById("start-button").onclick = function() {start()};
+  
+  function start(){
+    
+    document.getElementById("start-button").innerHTML = "Let's go!";
+    timestart = true;
+
+    setInterval(function(){
+      if(timestart == true){
+        seconds--;
+        if(seconds==-1){
+          
+        }else if(seconds < 10){
+          document.getElementById("seconds").innerHTML = "0"+seconds ;
+        }else{
+          document.getElementById("seconds").innerHTML = seconds;
+        }
+
+        if(seconds < 0){
+          minutes--;
+          document.getElementById("minutes").innerHTML = minutes+":";
+          seconds = 59;
+          document.getElementById("seconds").innerHTML = 59;
+        }
+
+      }
+  
+      if(minutes==0 && seconds==0){
+        if(timestart ==true){
+           alert("The time is over!")
+        }
+        timestart = false;
+      }
+
+    },1000)
+  } 
 });
+
